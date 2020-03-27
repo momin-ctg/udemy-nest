40263521cd1146469409bd93ed233ecd90eda50d
mominriyadh <riyadh@codesign.com.bd>
2020-03-27 16:04:50 +0600

    learn, write code and push

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,12 +1,13 @@
 import {Controller, Get} from '@nestjs/common';
 import {TaskService} from './task.service';
+import {Task} from './task.model';
 
 @Controller('task')
 export class TaskController {
     constructor(private taskService: TaskService) {
     }
 
-    @Get() getAllTask() {
+    @Get() getAllTask(): Task[] {
         return this.taskService.getAllTask();
     }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,10 +1,11 @@
 import {Injectable} from '@nestjs/common';
+import {Task} from "./task.model";
 
 @Injectable()
 export class TaskService {
-    private task = [];
+    private task:Task[] = [];
 
-    getAllTask() {
+    getAllTask(): Task[]  {
             return this.task
     }
 }
