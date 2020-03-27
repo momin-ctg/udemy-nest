4499bebb8a81c9bb1d0d3d265c1ab6346ef49eac
mominriyadh <riyadh@codesign.com.bd>
2020-03-27 16:49:53 +0600

    fix and deploy

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,13 +1,20 @@
-import {Controller, Get} from '@nestjs/common';
-import {TaskService} from './task.service';
-import {Task} from './task.model';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { Task } from './task.model';
 
 @Controller('task')
 export class TaskController {
-    constructor(private taskService: TaskService) {
-    }
+  constructor(private taskService: TaskService) {
+  }
 
-    @Get() getAllTask(): Task[] {
-        return this.taskService.getAllTask();
-    }
+  @Get() getAllTask(): Task[] {
+    return this.taskService.getAllTask();
+  }
+
+  @Post()
+  createTask(@Body('title') title: string,
+             @Body('description') description: string) {
+    console.log('title', title);
+    console.log('description', description);
+  }
 }
